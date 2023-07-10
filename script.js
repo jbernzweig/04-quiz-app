7b04bd183414c3215c437fd7c1fa5707dfb52a69
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-07-10 11:01:34 -0400

    basic variables added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
 const questions = [
     {
         question:'Javascript is a programming language',
