1be6a3055eaeab67f352701e32ba2fafeca5576a
Jason Bernzweig <jasonbernzweig@Jasons-Air.lan>
2023-07-10 11:00:13 -0400

    Questions Added

---
+++ script.js
@@ -0,0 +1,41 @@
+const questions = [
+    {
+        question:'Javascript is a programming language',
+        answers: [
+            { text: 'True', correct: true },
+            { text: 'False', correct: false }
+        ]
+    },
+    {
+        question:'What coding bootcamp is the best?',
+        answers: [
+            { text: 'Ohio State', correct: true },
+            { text: 'Michigan', correct: false },
+            { text: 'Colorado', correct: false},
+            { text: 'Alaska', correct: false},
+        ]
+    },
+    {
+        question:'You can learn programming for free or at little expense',
+        answers: [
+            { text: 'True', correct: true },
+            { text: 'False', correct: false }
+        ]
+    },
+    {
+        question:'Java is the same as Javascript',
+        answers: [
+            { text: 'False', correct: true },
+            { text: 'True', correct: false }
+        ]
+    },
+    {
+        question:'How do you like your coffee',
+        answers: [
+            { text: 'Scripted Javachip', correct: true },
+            { text: 'Black', correct: true },
+            { text: 'Frappe', correct: false },
+            { text: 'Anything will do', correct: true }
+        ]
+    }
+]
